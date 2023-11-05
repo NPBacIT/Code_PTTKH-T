13e7f01445393fa3db788966be3f53a50709676d
bacnp <nguyenphuongbaca3k46@gmail.com>
2023-11-05 18:32:02 +0700

    feat: tao moi trương

---
+++ banvekhugiaitri_nhom4_pttkhdt/banvekhugiaitri_nhom4_pttkhdt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace banvekhugiaitri_nhom4_pttkhdt.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
